64642ca3ac6de3f8bf93382210bfdf195816ee17
Erik-Koning <erik@koning.ca>
2019-07-16 23:08:59 +0100

    2.91

--- startTmuxDebug.sh
+++ startTmuxDebug.sh
@@ -4,14 +4,13 @@ tmux new-session -d -s session1
 tmux rename-window 'debug gateway window'
 
 tmux split-window -v
+tmux send-keys 'mosquitto_sub -v -t "gateway/#" -h 172.105.0.73 -p 1883' Enter
 tmux select-pane -t 0
 tmux send-keys 'sudo tcpdump -AUq -i lo port 1700' Enter
 tmux split-window -h
 tmux send-keys 'journalctl -f -n 100 -u doppl-gateway.service' Enter
-tmux send-keys tmux select-pane -t 2
-tmux send-keys 'mosquitto_sub -v -t "gateway/#" -h 172.105.0.73 -p 1883' Enter
+tmux select-pane -t 2
 tmux split-pane -h
-tmux select-plane -t 3
 tmux send-keys 'journalctl -f -n 100 -u lora-gateway-bridge.service' Enter
 
 
